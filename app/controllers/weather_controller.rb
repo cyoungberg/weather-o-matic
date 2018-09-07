2a17805ac9cbfae4cb9b330eea26a8c3227762e2
Chad Youngberg <chadyoungberg@gmail.com>
2018-09-07 16:39:31 -0700

    Wire everything up using a secure credential

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,10 @@
 class WeatherController < ApplicationController
   def index
-    
+    @city = params[:city]
+
+    weather_client = OpenWeatherMap::Client.new(Rails.application.credentials.open_weather_map_api_key)
+    weather = weather_client.get_weather(@city)
+
+    @temp_farenheit = TempConverter.from_kelvin(weather.main.temp).to_farenheit
   end
 end
